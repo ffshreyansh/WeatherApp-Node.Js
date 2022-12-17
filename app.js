f24f1456923e4ec6ce96d15329c39fbd3060c7c1
Shreyansh Kumar <shreyanshkumar058@gmail.com>
2022-12-17 20:46:31 +0530

    weather app init

---
+++ app.js
@@ -0,0 +1,43 @@
+const express = require("express");
+const https = require("https");
+const bodyParser = require("body-parser");
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get("/", function (req, res) {
+
+    res.sendFile(__dirname + "/index.html");
+
+});
+
+app.post("/", function (req, res) {
+    const query = req.body.cityName
+    const apiKey = "8ef95f6e28dd5057a452138bd5ca6e61"
+    const unit = "metric"
+    const url = "https://api.openweathermap.org/data/2.5/weather?q=" + query + "&appid=" + apiKey + "&units=" + unit;
+    https.get(url, function (response) {
+        console.log(response.statusCode);
+
+        response.on("data", function (data) {
+            const weatherData = JSON.parse(data)
+            const temp = weatherData.main.temp
+            const weatherDescription = weatherData.weather[0].description
+            const icon = weatherData.weather[0].icon
+            const imageUrl = "http://openweathermap.org/img/wn/" + icon + "@2x.png"
+            res.write("<p>The weather is currently " + weatherDescription + "</p>");
+            res.write("<h1>The temperature in "+query+" is: " + temp + " degree Celcius</h1>");
+            res.write("<img src=" + imageUrl + "></img>")
+
+            res.send();
+
+        });
+
+    });
+})
+
+
+
+app.listen(3000, function () {
+    console.log("server on 3000 is live")
+});
